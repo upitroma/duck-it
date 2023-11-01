a9ecbace6e47b37d30d1f35624bc472f88603b5c
upitroma <55750919+upitroma@users.noreply.github.com>
2023-11-01 12:38:54 -0400

    add modify endpoint

--- duck_api/main.js
+++ duck_api/main.js
@@ -28,36 +28,42 @@ con.connect(function(err) {
 
 
 var app = express();
+app.use(express.json());
 // app.use(cors());
 
 
-app.get("/api/modify/",function(req,res){
-    if(!req.query.id || req.query.id=="null"){
-        res.send({"status":"ERR: no id provided"})
-        return
-    }
+app.post('/api/modify/', (req, res) => {
+    let data = req.body;
+    // check if duck exists
+    con.query("SELECT * FROM duck_it.ducks WHERE id = '"+data.id+"'", function (err, result, fields) {
+        if (err) throw err;
+        //if duck exists
+        if (result.length==1){
+            //make sure all fields are present and valid integers
+            if (Number.isInteger(data.strength) && Number.isInteger(data.perception) && Number.isInteger(data.endurance) && Number.isInteger(data.charisma) && Number.isInteger(data.intelligence) && Number.isInteger(data.agility) && Number.isInteger(data.luck)){
+                //update duck
+                con.query("UPDATE duck_it.ducks SET strength = '"+data.strength+"', perception = '"+data.perception+"', endurance = '"+data.endurance+"', charisma = '"+data.charisma+"', intelligence = '"+data.intelligence+"', agility = '"+data.agility+"', luck = '"+data.luck+"' WHERE id = '"+data.id+"'", function (err, result, fields) {
+                    if (err) throw err;
+                    res.send('duck updated: ' + JSON.stringify(data));
+                });
+            }
+            else{
+                res.status(400).send({"duckName":"ERR: MALFORMED_DATA"})
+            }
 
-    if(!req.query.id || req.query.id=="null"){
-        res.send({"status":"ERR: no id provided"})
-        return
-    }
 
-    
-});
+            
+        }
+        else{
+            res.status(400).send({"duckName":"ERR: DUCK_DOES_NOT_EXIST"})
+        }                
+    });
+})
+
 
 app.get("/api/",function(req,res){
-    if(!req.query.id || req.query.id=="null"){
-
-        // count total ducks
-        con.query("SELECT COUNT(*) AS count FROM duck_it.ducks", function (err, result) {
-            if(err) throw err;
-
-            // console.log(result[0].count)
-            totalCount = result[0].count;
-
-            res.send({"duckCount":totalCount});
-                
-        });
+    if(!req.query.id || req.query.id=="null" || req.query.id=="undefined" || req.query.id==""){
+        res.status(400).send({"duckName":"ERR: DUCK_DOES_NOT_EXIST"})
     }
     else{
         //fix sql injection
@@ -92,12 +98,12 @@ app.get("/api/",function(req,res){
                     });
                 }
                 else{
-                    res.status(404).send({"duckName":"ERR: DUCK_DOES_NOT_EXIST"})
+                    res.status(400).send({"duckName":"ERR: DUCK_DOES_NOT_EXIST"})
                 }                
             });
         }
         else{
-            res.status(404).send({"duckName":"ERR: MALFORMED_ID"})
+            res.status(400).send({"duckName":"ERR: MALFORMED_ID"})
         }
 
     }
